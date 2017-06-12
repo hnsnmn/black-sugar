b2a2d5e3b0622a0ffd1e11d879b82a89866cc42b
hnsnmn <hnsnmn@gmail.com>
2017-06-12 15:34:18 +0900

    mustach template 사용 및 controller 구현

---
+++ black-sugar/src/main/java/com/sugar/web/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.sugar.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/helloworld")
+	public String welcom(String name, Model model) {
+		model.addAttribute("name", name);
+		return "welcome";
+	}
+}
