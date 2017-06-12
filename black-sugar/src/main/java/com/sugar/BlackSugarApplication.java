44b6d4431e106a88293862c2fd3c18970ab0c1ca
hnsnmn <hnsnmn@gmail.com>
2017-06-13 08:49:49 +0900

    embeded tomcat run

---
+++ black-sugar/src/main/java/com/sugar/BlackSugarApplication.java
@@ -0,0 +1,12 @@
+package com.sugar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackSugarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackSugarApplication.class, args);
+	}
+}
