128f251685f7784f2036815dcf2ecaff2e7105f6
hnsnmn <hnsnmn@gmail.com>
2017-06-13 09:49:11 +0900

    목록 화면 구현 및 회원 가입 데이터 메모리에서 저장

--- black-sugar/src/main/java/com/sugar/web/UserController.java
+++ black-sugar/src/main/java/com/sugar/web/UserController.java
@@ -1,14 +1,27 @@
 package com.sugar.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	static List<User> userList = new ArrayList<User>();
 	
 	@PostMapping("/create")
 	public String create(User user) {
-		System.out.println(user);
-		return "index";
+		userList.add(user);
+		return "redirect:/list";
+	}
+	
+	
+	@GetMapping("list")
+	public String list(Model model) {
+		model.addAttribute("users", userList);
+		return "list";
 	}
 }
