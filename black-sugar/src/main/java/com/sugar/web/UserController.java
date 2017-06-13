da9ce8d0551f2209ee35a3784f81369ffc90e383
hnsnmn <hnsnmn@gmail.com>
2017-06-13 17:31:43 +0900

    회원 정보 수정 구현

--- black-sugar/src/main/java/com/sugar/web/UserController.java
+++ black-sugar/src/main/java/com/sugar/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,5 +32,20 @@ public class UserController {
 	@GetMapping("/form")
 	public String form() {
 		return "/user/form";
+	}
+
+	@GetMapping("/{id}/form")
+	public String updatedform(@PathVariable Long id, Model model) {
+		model.addAttribute("user", userRepository.findOne(id));
+		return "/user/updatedForm";
+	}
+	
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		System.out.println(user);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
