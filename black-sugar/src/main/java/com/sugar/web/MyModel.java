b1d3e9048ddcf4ed2772472889078be6b1789a00
hnsnmn <hnsnmn@gmail.com>
2017-06-13 13:21:11 +0900

    mustach 문법 테스트

---
+++ black-sugar/src/main/java/com/sugar/web/MyModel.java
@@ -0,0 +1,17 @@
+package com.sugar.web;
+
+public class MyModel {
+	private String name;
+
+	public MyModel(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
--- black-sugar/src/main/java/com/sugar/web/WelcomeController.java
+++ black-sugar/src/main/java/com/sugar/web/WelcomeController.java
@@ -1,5 +1,8 @@
 package com.sugar.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController {
 	
 	@GetMapping("/helloworld")
-	public String welcom(String name, Model model) {
-		model.addAttribute("name", name);
+	public String welcom(Model model) {
+		List<MyModel> repo = Arrays.asList(new MyModel("resque"), new MyModel("hub"), new MyModel("rip"));
+		model.addAttribute("repo", repo);
 		return "welcome";
 	}
 	
