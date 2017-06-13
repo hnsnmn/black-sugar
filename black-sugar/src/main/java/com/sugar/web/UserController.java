4016a9528208bf142df34a76ae3191ac376bed4c
hnsnmn <hnsnmn@gmail.com>
2017-06-13 16:37:02 +0900

    rest api url로 변경

--- black-sugar/src/main/java/com/sugar/web/UserController.java
+++ black-sugar/src/main/java/com/sugar/web/UserController.java
@@ -5,23 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sugar.domain.User;
 import com.sugar.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	@Autowired
 	private UserRepository userRepository ;
 	
-	@PostMapping("/user/create")
+	@PostMapping("")
 	public String create(User user) {
 		userRepository.save(user);
-		return "redirect:/user/list";
+		return "redirect:/users";
 	}
 	
 	
-	@GetMapping("/user/list")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
