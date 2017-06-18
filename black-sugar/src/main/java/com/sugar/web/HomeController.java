c4668ebe6b33c7b3f28aa11b33efce215b62800e
{hongseongmin} <{hnsnmn@gmail.com}>
2017-06-18 22:52:53 +0900

    목록 화면 구현

--- black-sugar/src/main/java/com/sugar/web/HomeController.java
+++ black-sugar/src/main/java/com/sugar/web/HomeController.java
@@ -1,13 +1,20 @@
 package com.sugar.web;
 
+import com.sugar.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@GetMapping("")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("questions", questionRepository.findAll());
 		return "index";
 	}
 }
