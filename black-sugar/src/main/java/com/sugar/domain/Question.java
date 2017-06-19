d8a74e033018243f95aa7fbd08c68ad4ecbc2c3c
{hongseongmin} <{hnsnmn@gmail.com}>
2017-06-19 23:04:22 +0900

    DB 날짜 등록시 Local의 날짜를 자동으로변경해주는 클스 작성

--- black-sugar/src/main/java/com/sugar/domain/Question.java
+++ black-sugar/src/main/java/com/sugar/domain/Question.java
@@ -8,6 +8,7 @@ import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 /**
@@ -24,6 +25,8 @@ public class Question {
     private User writer;
     
     private String title;
+
+    @Lob
     private String contents;
     private LocalDateTime createdDate;
 
--- black-sugar/src/main/resources/import.sql
+++ black-sugar/src/main/resources/import.sql
@@ -1,2 +1,4 @@
-INSERT INTO USER (user_id, password, user_name, email) VALUES ('hnsnmn', '1111', '홍성민', 'aaa@naver.com');
-INSERT INTO USER (user_id, password, user_name, email) VALUES ('test', '1111', '테스터', 'bbb@naver.com');
+INSERT INTO USER (id, user_id, password, user_name, email) VALUES (1, 'hnsnmn', '1111', '홍성민', 'aaa@naver.com');
+INSERT INTO USER (id, user_id, password, user_name, email) VALUES (2, 'test', '1111', '테스터', 'bbb@naver.com');
+
+INSERT INTO QUESTION (ID, WRITER_ID, TITLE, CONTENTS, CREATED_DATE) VALUES (1, 1, '노력은 절대 배신하지 않는다.', '이건 불변의 진리다.',CURRENT_TIMESTAMP());
